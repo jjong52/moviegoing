33f749606984415c869a135b1b18dd80c249be7c
2jjong <108912080+jjong52@users.noreply.github.com>
2023-05-10 16:50:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, jsonify
-import random
+from flask import Flask, render_template, request, jsonify, redirect
 app = Flask(__name__)
 
+from bson.objectid import ObjectId
+
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.32ylit8.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://sparta:test@cluster0.awfowzp.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 import requests
@@ -13,58 +14,96 @@ from bs4 import BeautifulSoup
 def home():
     return render_template('index.html')
 
-@app.route("/movie", methods=["POST"])
-def movie_post():
+@app.route("/movies/new", methods=["GET"])
+def get_write():
+
+   return render_template('write.html')
+
+@app.route("/movies/new", methods=["POST"])
+def post_write():
     url_receive = request.form['url_give']
     comment_receive = request.form['comment_give']
     star_receive = request.form['star_give']
 
+    
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive,headers=headers)
-
     soup = BeautifulSoup(data.text, 'html.parser')
 
     ogtitle = soup.select_one('meta[property="og:title"]')['content']
     ogimage = soup.select_one('meta[property="og:image"]')['content']
     ogdesc = soup.select_one('meta[property="og:description"]')['content']
-
-    # 사용자가 입력한 url에서 movie_id 뽑기
-    movieid = int(url_receive[-5:])
-
-    print(movieid)
     
-    doc = { 
-        'title':ogtitle,
-        'desc':ogdesc,
-        'image':ogimage, 
-        'comment':comment_receive,
-        'star':star_receive,
-        'id':movieid
+    doc = {
+       'title' : ogtitle,
+       'desc' : ogdesc,
+       'image' : ogimage,
+       'url' : url_receive,
+       'comment' : comment_receive,
+       'star' : star_receive,
     }
+    
     db.movies.insert_one(doc)
 
-    return jsonify({'msg':'저장완료!'})
+    return redirect('/')
 
 @app.route("/movie", methods=["GET"])
-def movie_get(): 
-    all_movies = list(db.movies.find({},{'_id':False}))
+def movie_get():
+    all_movies = list(db.movies.find({},{'_id':True, 'title':True, 'desc':True, 'image':True, 'star':True, 'comment':True}))
+    
+    # ObjectId 값을 String 형식으로 변환
+    for movie in all_movies:
+        movie['_id'] = str(movie['_id'])
+        
     return jsonify({'result':all_movies})
+ 
+@app.route("/view/<id>", methods=["GET"])
+def one_find_movie(id):
+    
+    find_movie = db.movies.find_one({"_id": ObjectId(id)})
+    find_movie['_id'] = str(find_movie['_id'])
+    
+    find_id = db.movies.find_one({'_id' : ObjectId(id)},{'id':True})
+
+    
+    return render_template('view.html', movie=find_movie, find_id=find_id)
+
+@app.route("/update/<id>", methods=["GET"])
+def update_get(id):
+    
+    find_movie = db.movies.find_one({"_id": ObjectId(id)})
+    find_movie['_id'] = str(find_movie['_id'])
+    
+    find_id = db.movies.find_one({'_id' : ObjectId(id)},{'id':True})
+    
+    return render_template('update.html', movie=find_movie, find_id=find_id)
+
+@app.route("/update/<id>", methods=["POST"])
+def update_post(id):
+    
+    comment_receive = request.form['comment_give']
+    star_receive = request.form['star_give']
+
+    
+    find_movie = db.movies.find_one({"_id": ObjectId(id)})
+    find_movie['_id'] = str(find_movie['_id'])
+    
+    db.movies.update_one({'_id': ObjectId(id)},{'$set':{'comment':comment_receive}})
+    db.movies.update_one({'_id': ObjectId(id)},{'$set':{'star':star_receive}})
+
+    return redirect('/view/' + str(id))
 
 
+# 삭제
+@app.route("/delete/<id>", methods=["POST"])
+def delete_post(id):
+    find_movie = db.movies.find_one({"_id": ObjectId(id)})
+    find_movie['_id'] = str(find_movie['_id'])
+    
+    db.movies.delete_one({'_id': ObjectId(id)})
 
-#글쓰기 페이지
-@app.route('/create/')
-def create():
-    return '글쓰기 페이지'
+    return redirect('/')
 
-
-#상세 페이지
-@app.route('/read/<id>/')
-def read(id):
-    return 'Read '+id
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
-
-
-
